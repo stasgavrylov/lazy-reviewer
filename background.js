a973ea9a0f83e1c7dcafbc44a6b65b60ff8d5f72
Stanislav Gavrylov <stasgavrylov@gmail.com>
2016-11-25 11:08:55 +0200

    adds basic initialization failure handling

--- background.js
+++ background.js
@@ -18,15 +18,25 @@ const hostFilters =
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(data) {
   chrome.storage.local.set({ initialized: false })
+  var initCounter = 0
 
   const interval = setInterval(function() {
     chrome.storage.local.get('initialized', function({ initialized }) {
       if (initialized) return clearInterval(interval)
 
+      // Give up after 10 tries
+      if (initCounter > 10) {
+        console.error('Unable to initialize extension. Try to reload the page')
+        return clearInterval(interval)
+      }
+
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        if (!tabs[0]) return
         chrome.tabs.sendMessage(tabs[0].id, data)
       })
+
+      initCounter++
     })
   }, 500)
 },
-hostFilters);
+hostFilters)
